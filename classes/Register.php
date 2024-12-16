548736c517cedc8e39ef1ca7796e30a2d443c2db
Shanya-TS <168742933+Shanya-TS@users.noreply.github.com>
2024-12-16 07:25:26 +0530

    Add files via upload

--- classes/Register.php
+++ classes/Register.php
@@ -13,6 +13,7 @@ class Register{
 
     public function __construct ( $firstName, $lastName, $tp, $userName, $email, $password,$img_path){
         $this->firstName = $firstName;
+        $this->lastName = $lastName;
         $this->tp = $tp;
         $this->userName = $userName;
         $this->email = $email;
@@ -24,15 +25,15 @@ class Register{
         try {
             $db = new dbConnection();
             $db->dbconnt();
-            $queary = "INSERT INTO customer (fName,lName,username,password,contact,email,img_path)VALUES (?,?,?,?,?,?,?)";
+            $queary = "INSERT INTO customer (CustomerId,fName,lName,username,password,contact,email,img_path)VALUES (?,?,?,?,?,?,?,?)";
             $stmt= $db->preQuery($queary);
-            $stmt->bindValue(1,$this->firstName);
-            $stmt->bindValue(2,$this->lastName);
-            $stmt->bindValue(3,$this->userName);
-            $stmt->bindValue(4,$this->password);
-            $stmt->bindValue(5,$this->tp);
-            $stmt->bindValue(3,$this->email);
-            $stmt->bindValue(4,$this->img_path);
+            $stmt->bindValue(2,$this->firstName);
+            $stmt->bindValue(3,$this->lastName);
+            $stmt->bindValue(4,$this->userName);
+            $stmt->bindValue(5,$this->password);
+            $stmt->bindValue(6,$this->tp);
+            $stmt->bindValue(7,$this->email);
+            $stmt->bindValue(8,$this->img_path);
 
             $stmt->execute();
             return ($stmt->rowCount() > 0);
